711af13b51dd14f19ab71c3784e6a9b83bf4ba58
Awe <hilongjw@gmail.com>
2016-06-14 15:28:14 +0800

    fix: menu bg color

--- example/App.vue
+++ example/App.vue
@@ -1,32 +1,31 @@
 <style lang="scss">
 
 html,body{
-        height: 100%;
-        margin: 0;
-    }
-    .container{
-        height: 100%;
-        position: relative;
-        overflow: hidden;
-    }
-    .sidebar{
-        width: 20%;
-        background: black;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-    .content{
-        width: 100%;
-        padding-left: 20%;
-        height: 100%;
-    }
+    height: 100%;
+    margin: 0;
+}
+.container{
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+}
+.sidebar{
+    width: 20%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-right: 1px solid #f7f7f7;
+}
+.content{
+    width: 100%;
+    padding-left: 20%;
+    height: 100%;
+}
 .ex-container {
     display: flex;
 }
 .ex-menu-container {
-    width: 20%;
     min-height: 40rem;
 }
 .ex-menu-item {

9454d185703538d944ddd20dbf3879ceff29ea4f
Awe <hilongjw@gmail.com>
2016-06-14 10:41:24 +0800

    update: eslint intent config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": ["error", 4]
   }
 }

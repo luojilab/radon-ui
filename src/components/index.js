83c49dcae90746811617e5a869f3d54c38db0d9e
Awe <hilongjw@gmail.com>
2016-06-16 10:30:37 +0800

    add: checkbox to form

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import formItem from './form/formItem.vue'
 import radonButton from './basic/button.vue'
 import radonButtonGroup from './basic/buttonGroup.vue'
-import radonCheckbox from './basic/checkbox.vue'
+import radonCheckbox from './form/checkbox.vue'
 import radonText from './form/textfield.vue'
 import radonRadio from './form/radio.vue'
 

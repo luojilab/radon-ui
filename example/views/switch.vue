af5d338fc5501592e4e444af9cffa550029ccb13
Awe <hilongjw@gmail.com>
2016-06-18 20:53:53 +0800

    add: switch doc

---
+++ example/views/switch.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="ex-content">
+    <div class="ex-card">
+    <mark>
+        <textarea class="ex-mark-text">
+# Switch 开关
+
+## 代码
+```javascript
+export default { 
+  template: `<radon-switch :checked.sync="switchA" size="small"></radon-switch>
+            <radon-switch :checked.sync="switchA"></radon-switch>`,
+  data () {
+        return {
+            switchA: false
+        }
+    },
+    components: {
+        radonSwitch
+    }
+}
+```
+## 示例
+
+        </textarea>
+    </mark>
+        <p>
+            <radon-switch :checked.sync="switchA" size="small"></radon-switch>
+            <radon-switch :checked.sync="switchA"></radon-switch>
+        </p>
+    </div>
+</div>
+</template>
+<script>
+import mark from './mark.vue'
+import {
+    radonSwitch
+} from '../../src/components/index'
+
+export default {
+    props: {
+        createModal: Function
+    },
+    data () {
+        return {
+            switchA: false
+        }
+    },
+    components: {
+        radonSwitch,
+        mark
+    }
+}
+</script>

0e06b2c1c2795929931e696370acb65f1d8b0b37
Awe <hilongjw@gmail.com>
2016-06-18 20:32:35 +0800

    add: button doc

--- example/views/button.vue
+++ example/views/button.vue
@@ -1,12 +1,20 @@
 <template>
 <div class="ex-content">
-    <h2>Button 按钮</h2>
-    <p>按钮用于开始一个即时操作。</p>
     <div class="ex-card">
-        <h3>按钮类型</h3>
-        <p class="ex-code">
-            &lt;radon-button type=&quot;primary&quot;&gt;primary&lt;/radon-button&gt;
-        </p>
+    <mark>
+        <textarea class="ex-mark-text">
+# Button 按钮
+按钮用于开始一个即时操作。
+
+### 按钮类型
+```html
+<radon-button>default</radon-button>
+<radon-button type="primary">primary</radon-button>
+<radon-button type="ghost">ghost</radon-button>
+
+```
+        </textarea>
+    </mark>
         <p>
             <radon-button>default</radon-button>
             <radon-button type="primary">primary</radon-button>
@@ -14,20 +22,17 @@
         </p>
     </div>
     <div class="ex-card">
-        <h3>图标</h3>
-        <p class="ex-code">
-           &lt;radon-button type=&quot;icon&quot;&gt;&lt;i class=&quot;ion-upload&quot;&gt;&lt;/i&gt;&lt;/radon-button&gt;
-        </p> 
-        </p>
-        <p>
-            <radon-button type="icon"><i class="ion-upload"></i></radon-button>
-        </p>
-    </div>
-    <div class="ex-card">
-        <h3>按钮尺寸</h3>
-        <p class="ex-code">
-            size="large" or size="small"
-        </p>
+        <mark>
+            <textarea class="ex-mark-text">
+### 按钮尺寸
+```html
+<radon-button type="primary" size="large">large</radon-button>
+<radon-button type="primary">common</radon-button>
+<radon-button type="primary" size="small">small</radon-button>
+
+```
+            </textarea>
+        </mark>
         <p>
             <radon-button type="primary" size="large">large</radon-button>
             <radon-button type="primary">common</radon-button>
@@ -35,14 +40,35 @@
         </p>
     </div>
     <div class="ex-card">
-        <h3>按钮组合</h3>
-        <p class="ex-code">
-            &lt;radon-button-group&gt; <br/>
-                &lt;radon-button type=&quot;ghost&quot;&gt;common&lt;/radon-button&gt;<br/>
-                &lt;radon-button type=&quot;ghost&quot;&gt;common&lt;/radon-button&gt;<br/>
-                &lt;radon-button type=&quot;ghost&quot;&gt;common&lt;/radon-button&gt;<br/>
-            &lt;/radon-button-group&gt;
+    <mark>
+        <textarea class="ex-mark-text">
+### 图标
+```html
+<radon-button type="icon"><i class="ion-upload"></i></radon-button>
+<radon-button><i class="ion-upload"></i>上传</radon-button>
+
+```
+        </textarea>
+    </mark>
+        <p>
+            <radon-button type="icon"><i class="ion-upload"></i></radon-button>
+            <radon-button><i class="ion-upload"></i>上传</radon-button>
         </p>
+    </div>
+    <div class="ex-card">
+        <mark>
+            <textarea class="ex-mark-text">
+### 按钮组合
+```html
+<radon-button-group>
+    <radon-button type="ghost">common</radon-button>
+    <radon-button type="ghost">common</radon-button>
+    <radon-button type="ghost">common</radon-button>
+</radon-button-group>
+
+```
+            </textarea>
+        </mark>
         <p>
             <radon-button-group>
                 <radon-button type="ghost">common</radon-button>
@@ -52,10 +78,15 @@
         </p>
     </div>
     <div class="ex-card">
-        <h3>loading</h3>
-        <p class="ex-code">
-            &lt;radon-button type=&quot;ghost&quot; :loading=&quot;true&quot;&gt;save&lt;/radon-button&gt;
-        </p>
+        <mark>
+            <textarea class="ex-mark-text">
+### loading
+```html
+<radon-button type="ghost" :loading="true">save</radon-button>
+
+```
+            </textarea>
+        </mark>
         <p>
             <radon-button type="ghost" :loading="true">save</radon-button>
         </p>
@@ -63,6 +94,7 @@
 </div>
 </template>
 <script>
+import mark from './mark.vue'
 import {
     formItem,
     radonButton,
@@ -83,7 +115,8 @@ export default {
     components: {
         formItem,
         radonButton,
-        radonButtonGroup
+        radonButtonGroup,
+        mark
     }
 }
 </script>

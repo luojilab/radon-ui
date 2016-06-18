d0e4f937d2603a6e56a06c2b3b5415a708d1ad4d
Awe <hilongjw@gmail.com>
2016-06-18 21:30:36 +0800

    add: select doc

---
+++ example/views/select.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="ex-content">
+    <div class="ex-card">
+    <mark>
+        <textarea class="ex-mark-text">
+# Select 选择器
+
+## 代码
+```javascript
+export default {
+    template: '<radon-select :value.sync="selectProvince.value" :options.sync="selectProvince.options"></radon-select>',
+    data () {
+        return {
+            selectProvince: {
+                value: {},
+                options: [{
+                    selected: false,
+                    disabled: false,
+                    value: '四川',
+                    id: 1
+                }, {
+                    selected: false,
+                    disabled: false,
+                    value: '北京',
+                    id: 2
+                }, {
+                    selected: false,
+                    disabled: false,
+                    value: '广东',
+                    id: 3
+                }]
+            }
+        }
+    },
+    components: {
+        rdSelect
+    }
+}
+```
+## 示例
+
+        </textarea>
+    </mark>
+        <p>
+            <rd-select :value.sync="selectProvince.value" :options.sync="selectProvince.options"></rd-select>
+        </p>
+    </div>
+</div>
+</template>
+<script>
+import mark from './mark.vue'
+import {
+    rdSelect
+} from '../../src/components/index'
+
+export default {
+    props: {
+        createModal: Function
+    },
+    data () {
+        return {
+            selectProvince: {
+                value: {},
+                options: [{
+                    selected: false,
+                    disabled: false,
+                    value: '四川',
+                    id: 1
+                }, {
+                    selected: false,
+                    disabled: false,
+                    value: '北京',
+                    id: 2
+                }, {
+                    selected: false,
+                    disabled: false,
+                    value: '广东',
+                    id: 3
+                }]
+            }
+        }
+    },
+    components: {
+        rdSelect,
+        mark
+    }
+}
+</script>

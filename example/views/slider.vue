55fffcd4a84f5cc1b0fb15987b01e7b526ae2ea1
Awe <hilongjw@gmail.com>
2016-06-18 20:53:31 +0800

    add: slider doc

---
+++ example/views/slider.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="ex-content">
+    <div class="ex-card">
+    <mark>
+        <textarea class="ex-mark-text">
+# Slider 滑动输入条
+
+## 代码
+```javascript
+export default {
+    template: '<rd-slider :value.sync="slider"></rd-slider>',
+    data () {
+        return {
+            slider: 43,
+        }
+    },
+    components: {
+        rdSlider
+    }
+}
+```
+## 示例
+
+        </textarea>
+    </mark>
+        <p>
+            <rd-slider :value.sync="slider"></rd-slider>
+        </p>
+    </div>
+</div>
+</template>
+<script>
+import mark from './mark.vue'
+import {
+    rdSlider
+} from '../../src/components/index'
+
+export default {
+    props: {
+        createModal: Function
+    },
+    data () {
+        return {
+            slider: 43
+        }
+    },
+    components: {
+        rdSlider,
+        mark
+    }
+}
+</script>

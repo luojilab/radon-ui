930a4e116dc76267395db0f38e06a2507c698453
Awe <hilongjw@gmail.com>
2016-06-18 20:54:48 +0800

    add: add textfiel do

---
+++ example/views/textfield.vue
@@ -0,0 +1,96 @@
+<template>
+<div class="ex-content">
+    <div class="ex-card">
+    <mark>
+        <textarea class="ex-mark-text">
+# Textfield 输入框
+
+## 代码
+```javascript
+export default {
+    data () {
+        return {
+            form: {
+                user: {
+                    value: '',
+                    placeHolder: 'input here',
+                    title: '用户名:',
+                    state: 'default',
+                    tip: ''
+                }
+            }
+        }
+    },
+    components: {
+        radonText,
+    },
+    methods: {
+        userInputing () {
+            this.form.user.state = 'loading'
+            setTimeout(() => {
+                if (this.form.user.value.length < 6) {
+                    this.form.user.state = 'failed'
+                    this.form.user.tip = '该用户名已被使用'
+                } else {
+                    this.form.user.state = 'default'
+                    this.form.user.tip = ''
+                }
+            }, 2000)
+        }
+    }
+}
+```
+## 示例
+
+        </textarea>
+    </mark>
+        <p>
+            <radon-text :textfield="form.user" :input="userInputing"></radon-text>
+        </p>
+    </div>
+</div>
+</template>
+<script>
+import mark from './mark.vue'
+import {
+    radonText
+} from '../../src/components/index'
+
+export default {
+    props: {
+        createModal: Function
+    },
+    data () {
+        return {
+            form: {
+                user: {
+                    value: '',
+                    placeHolder: 'input here',
+                    title: '用户名:',
+                    state: 'default',
+                    tip: ''
+                }
+            }
+        }
+    },
+    components: {
+        radonText,
+        mark
+    },
+    methods: {
+        userInputing () {
+            this.form.user.state = 'loading'
+
+            setTimeout(() => {
+                if (this.form.user.value.length < 6) {
+                    this.form.user.state = 'failed'
+                    this.form.user.tip = '该用户名已被使用'
+                } else {
+                    this.form.user.state = 'default'
+                    this.form.user.tip = ''
+                }
+            }, 2000)
+        }
+    }
+}
+</script>

17ba04cc1e2418f096d3fc1c4edb387c82661391
Awe <hilongjw@gmail.com>
2016-06-19 10:11:57 +0800

    add: global notification

---
+++ src/components/presentation/notification.vue
@@ -0,0 +1,137 @@
+<style lang="scss">
+.rd-notification-container {
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+    z-index: 99;
+}
+.rd-notification {
+    position: relative;
+    width: 20rem;
+    background-color: #fff;
+    margin-right: 1rem;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.41);
+    border-radius: 4px;
+    padding: 1rem 1.5rem;
+    margin-bottom: 1rem;
+    &.success,
+    &.warning,
+    &.failed,
+    &.info {
+        padding-left: 4rem;
+    }
+    &.success {
+        .rd-notification-icon {
+            color: #87d068;
+        }
+    }
+    &.info {
+        .rd-notification-icon {
+            color: #2db7f5;
+        }
+    }
+    &.warning {
+        .rd-notification-icon {
+            color: #fa0;
+        }
+    }
+    &.failed {
+        .rd-notification-icon {
+            color: #f50;
+        }
+    }
+}
+.rd-notification-title {
+    font-size: .9rem;
+}
+.rd-notification-content {
+    color: #999;
+    font-size: .8rem;
+    line-height: 1.5;
+    margin: 0;
+}
+.rd-notification-close {
+    position: absolute;
+    top: .2rem;
+    right: .5rem;
+    font-size: .8rem;
+    color: #ccc;
+}
+.rd-notification-close:hover {
+    color: #969696;
+}
+.rd-notification-icon{
+    position: absolute;
+    top: 50%;
+    font-size: 2.5rem;
+    left: 1rem;
+    margin-top: -1.25rem;
+    line-height: 2.5rem;
+}
+.notification-transition {
+  transition: opacity .5s ease;
+}
+.notification-enter {
+  opacity: 0;
+}
+.notification-leave {
+  opacity: 0;
+  position: absolute;
+}
+.notification-move {
+  transition: transform .5s cubic-bezier(.55,0,.1,1);
+} 
+</style>
+<template>
+    <div class="rd-notification-container">
+        <div 
+            class="rd-notification" 
+            v-for="item in notifications"
+            :class="typeClass(item)"
+            transition="notification"
+        >
+            <span class="rd-notification-icon" :class="iconClass(item)"></span>
+            <span class="rd-notification-title">{{item.title}}</span>
+            <p class="rd-notification-content">{{item.content}}</p>
+            <span class="rd-notification-close ion-close-round" @click="closeItem(item)"></span>
+        </div>
+    </div>
+</template>
+<script>
+const ICON_MAP = {
+    'success': 'ion-checkmark-circled',
+    'failed': 'ion-close-circled',
+    'warning': 'ion-alert-circled',
+    'info': 'ion-information-circled'
+}
+export default {
+    props: {
+        notifications: Array
+    },
+    methods: {
+        typeClass (item) {
+            if (!item.type) {
+                return {
+                    'rd-notification-info': true
+                }
+            }
+            let classList = {}
+            classList[item.type] = true
+            return classList
+        },
+        iconClass (item) {
+            if (!item.type || !ICON_MAP[item.type]) {
+                return {
+                    'rd-notification-noicon': true
+                }
+            }
+            let classList = {}
+            classList[ICON_MAP[item.type]] = true
+            return classList
+        },
+        closeItem (item) {
+            this.notifications.$remove(item)
+        }
+    }
+}
+</script>

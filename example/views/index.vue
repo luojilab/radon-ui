3627dab0f025e93d902e2e0f5a8a03d1464c39c8
Awe <hilongjw@gmail.com>
2016-06-20 17:58:03 +0800

    add: clear fix

--- example/views/index.vue
+++ example/views/index.vue
@@ -1,5 +1,13 @@
+<style>
+    .red {
+        border: 1px solid red;
+        height: 2rem;
+    }
+</style>
 <template>
-	<div>
-		index
+	<div class="rd-row-flex">
+		<div class="rd-col-8 red"></div>
+        <div class="rd-col-8 red"></div>
+        <div class="rd-col-8 red"></div>
 	</div>
 </template>

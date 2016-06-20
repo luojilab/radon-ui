026d640abbcc52c4dfbb754dca45e5639dca06c8
Awe <hilongjw@gmail.com>
2016-06-20 18:26:23 +0800

    add: flex row col

--- example/views/index.vue
+++ example/views/index.vue
@@ -1,13 +1,27 @@
 <style>
-    .red {
-        border: 1px solid red;
-        height: 2rem;
-    }
+.red {
+    border: 1px solid red;
+    height: 2rem;
+}
 </style>
 <template>
-	<div class="rd-row-flex">
-		<div class="rd-col-8 red"></div>
-        <div class="rd-col-8 red"></div>
-        <div class="rd-col-8 red"></div>
-	</div>
+    <div>
+        <div class="rd-row-flex">
+            <div class="rd-col-4 red"></div>
+            <div class="rd-col-8 red"></div>
+            <div class="rd-col-2 red"></div>
+        </div>
+        <div class="rd-row-flex rd-row-flex-end">
+            <div class="rd-col-4 red"></div>
+            <div class="rd-col-8 red"></div>
+            <div class="rd-col-2 red"></div>
+        </div>
+        <div class="rd-row-flex rd-row-flex-center">
+            <div class="rd-col-4 red"></div>
+            <div class="rd-col-8 red"></div>
+            <div class="rd-col-2 red"></div>
+        </div>
+    </div>
 </template>
+<script>
+</script>

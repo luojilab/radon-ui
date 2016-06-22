fc87ec5b3160e4dccb85bdc9411c595eccd620d5
Awe <hilongjw@gmail.com>
2016-06-22 10:49:22 +0800

    update: modal docs

--- example/views/modal.vue
+++ example/views/modal.vue
@@ -3,9 +3,24 @@
     <div class="ex-card">
     <mark>
         <textarea class="ex-mark-text">
-# Cascader 级联选择
+# Modal 弹窗
+模态对话框。
+## 何时使用
 
-## 代码
+需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
+另外当需要一个简洁的确认框询问用户时
+```
+this.$Modal.create(title:String, content:String, onConfirm:Function, onCancle:Function)
+```
+## 示例
+        </textarea>
+    </mark>
+    <p>
+        <radon-button @click="open">开启对话框</radon-button>
+    </p>
+    <mark>
+        <textarea class="ex-mark-text">
+## 示例代码
 ```javascript
 export default {
     template: '<radon-modal :modal="modal"></radon-modal>',
@@ -48,13 +63,8 @@ export default {
     }
 }
 ```
-## 示例
-
         </textarea>
     </mark>
-        <p>
-            <radon-button @click="open">开启对话框</radon-button>
-        </p>
     </div>
 </div>
 </template>
@@ -78,10 +88,10 @@ export default {
                 '这里是标题',
                 '这里应该说点什么',
                 () => {
-                    window.alert('你点击了确认')
+                    this.$Notification.success('你点击了确认', '你点击了确认你点击了确认你点击了确认你点击了确认你点击了确认', 5000)
                 },
                 () => {
-                    window.alert('点点击了取消')
+                    this.$Notification.success('你点击了取消', '', 5000)
                 }
             )
         }

398315b2c1e3d1d254c71cacbf3c2a71e73fcf36
Awe <hilongjw@gmail.com>
2016-06-24 10:49:48 +0800

    change port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 9090;
+var port = 9898;
 
 // static assets served by express.static() for production
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(port, function () {
-    console.log('App (production) is now running on port 9090!');
+    console.log('App (production) is now running on port ' + port + '!');
 });

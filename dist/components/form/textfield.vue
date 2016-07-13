b67bf815cb82faa1e031905e2d46b407f8eca6d2
Awe <hilongjw@gmail.com>
2016-07-13 11:41:51 +0800

    update

--- dist/components/form/textfield.vue
+++ dist/components/form/textfield.vue
@@ -11,7 +11,7 @@
      }"
     >
         <input 
-            type="text" 
+            :type="type" 
             class="rd-textfield-input" 
             v-model="textfield.value" 
             :placeholder="textfield.placeHolder"
@@ -29,6 +29,12 @@ export default {
         textfield: {
             type: Object,
             required: true
+        },
+        type: {
+            type: String,
+            default () {
+                return 'text'
+            }
         },
         input: Function,
         change: Function

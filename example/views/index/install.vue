374273898f3f51cc32e7f060f21dd19406cd7fb9
Awe <hilongjw@gmail.com>
2016-07-17 19:46:16 +0800

    update: install docs

--- example/views/index/install.vue
+++ example/views/index/install.vue
@@ -11,16 +11,66 @@
 推荐使用 npm 的方式进行开发，不仅可在开发环境轻松调试，也可放心地在生产环境打包部署使用，享受整个生态圈和工具链带来的诸多好处。
 可以通过 npm 直接安装到项目中，使用 import 或 require 进行引用。
 
+```
+npm install radon-ui
+```
+
+> Use basic component
 
 ```
-// install
-npm install radon-ui
-
-
-// import 
-
 // in customer.vue
-import { rdDatePicker } from './radon-ui'
+import { rdDatePicker } from 'radon-ui'
+```
+
+> User global method
+
+```javascript
+// main.js
+import Vue from 'vue'
+import { RadonInstall } from 'radon-ui'
+
+RadonInstall(Vue)
+```
+
+```html
+<!-- set root Vue data & template -->
+<template>
+    <div class="container">
+        <router-view></router-view>
+        <radon-modal :modal="modal"></radon-modal>
+        <rd-notification :notifications="Notifications"></rd-notification>
+        <rd-preview :preview="preview"></rd-preview>
+    </div>
+</template>
+<script>
+import {
+    radonModal,
+    rdNotification
+} from '../src/components/index'
+
+export default {
+    data () {
+        return {
+            Notifications: [],
+            modal: {
+                show: false,
+                title: '',
+                content: '',
+                cancel: () => {},
+                confirm: () => {}
+            }
+        }
+    },
+    created () {
+        this.$Radon.setRoot(this)
+    },
+    components: {
+        radonModal,
+        rdNotification
+    }
+}
+</script>
+
 ```
 
 ## 使用 git submodule

4248d3515fe41ec424e3b781b6e03d4a7a1632ef
Awe <hilongjw@gmail.com>
2016-08-24 19:18:55 +0800

    build

--- dist/components/presentation/preview.vue
+++ dist/components/presentation/preview.vue
@@ -5,7 +5,16 @@
         <div class="rd-preview-close" @click="close">
             <i class="ion-close-round rd-preview-close-icon" ></i>
         </div>
-        <img class="rd-preview-img" v-if="preview.current.src" :src="preview.current.src" :alt="preview.current.title">
+        <img 
+            v-el:preview-img
+            class="rd-preview-img" 
+            v-if="preview.current.src" 
+            :style="{
+                'margin-top': marginTop + 'px'
+            }"
+            :src="preview.current.src" 
+            :alt="preview.current.title"
+        >
         <div class="rd-preview-title">
             {{preview.current.title}}
             {{preview.current.index + '/' + preview.list.length}}
@@ -30,7 +39,8 @@ export default {
     },
     data () {
         return {
-            $box: null
+            $box: null,
+            marginTop: 50
         }
     },
     ready () {
@@ -48,17 +58,25 @@ export default {
         close () {
             this.preview.show = false
         },
+        imgPosition () {
+            if (!this.$els.previewImg || !this.$el) return
+            this.$nextTick(() => {
+                this.marginTop = 0.5 * (this.$el.offsetHeight - this.$els.previewImg.offsetHeight)
+            })
+        },
         preAction () {
             let index = this.preview.list.indexOf(this.preview.current)
             if (index === 0) return
             index--
             this.preview.current = this.preview.list[index]
+            this.imgPosition()
         },
         nextAction () {
             let index = this.preview.list.indexOf(this.preview.current)
             if (index === this.preview.list.length - 1) return
             index++
             this.preview.current = this.preview.list[index]
+            this.imgPosition()
         }
     }
 }

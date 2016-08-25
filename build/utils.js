4c28fb9ee08bfb95b39b2dd80235d8db143172de
Awe <hilongjw@gmail.com>
2016-08-25 19:29:00 +0800

    build

--- build/utils.js
+++ build/utils.js
@@ -3,6 +3,7 @@ var config = require('../config')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 exports.assetsPath = function (_path) {
+  console.log(path.posix.join(config.build.assetsSubDirectory, _path))
   return path.posix.join(config.build.assetsSubDirectory, _path)
 }
 
--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     dev: path.resolve(__dirname, '../dist/dev.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static/',
-    assetsPublicPath: '',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.

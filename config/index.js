3dbcac11348cae800b14ff324131aec2be41ed48
Awe <hilongjw@gmail.com>
2016-08-30 14:28:00 +0800

    fix: font dist

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     dev: path.resolve(__dirname, '../dist/dev.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.

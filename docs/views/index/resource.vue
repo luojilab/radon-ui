d5f2148ea0a55e53d358586ce6c3ef9fec7f18f9
Awe <hilongjw@gmail.com>
2016-08-30 21:28:05 +0800

    add: resource docs

--- docs/views/index/resource.vue
+++ docs/views/index/resource.vue
@@ -5,6 +5,15 @@
         <textarea class="ex-mark-text">
 # 资源
 
+### Vuejs
+
+RadonUI 基于 Vuejs 开发，[Vue.js 中文文档](http://vuejs.org.cn/)
+
+### 图标 Icon
+
+RadonUI 使用 ionic 的 icon 集合 [<i class="ion-ionic"></i>  ionicons](http://ionicons.com/)
+
+
 
         </textarea>
     </mark>

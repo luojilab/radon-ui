939ccc0e8dc6b40290e6a2e9af7c1536ae98eb57
Awe <hilongjw@gmail.com>
2016-09-13 17:42:59 +0800

    add: settle detial

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var port = 9898;
+var express = require('express')
+var path = require('path')
+var app = express()
+var port = 9898
 
 // static assets served by express.static() for production
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.post('/upload', function (req, res) {
     console.log(res.files)
-    res.send({'success': true })
+    res.send({ 'success': true })
 })
 
 app.listen(port, function () {
-    console.log('App (production) is now running on port ' + port + '!');
-});
+    console.log('App (production) is now running on port ' + port + '!')
+})

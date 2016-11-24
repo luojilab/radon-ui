ba7505c0819e195a2bc9f34b0c7e40f554dfda35
Awe <hilongjw@gmail.com>
2016-11-24 16:08:24 +0800

    fix: webpack eslint intent

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,32 +7,32 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+    baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
-  devtool: '#eval-source-map',
-  vue: {
-    loaders: utils.cssLoaders({
-      sourceMap: false
-    }),
-    postcss: [require('precss')(), require("postcss-url")()]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
-    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      chunks: ['app'],
-      inject: true
-    })
-  ]
+    devtool: '#eval-source-map',
+    vue: {
+        loaders: utils.cssLoaders({
+            sourceMap: false
+        }),
+        postcss: [require('precss')(), require('postcss-url')()]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': config.dev.env
+        }),
+        // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            chunks: ['app'],
+            inject: true
+        })
+    ]
 })

a5a2ba4e6d950e7dcf1f719a6dca00ef3f6bd743
Awe <hilongjw@gmail.com>
2016-11-24 16:18:02 +0800

    add: tree node build

---
+++ dist/components/common/tree.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <section class="rd-tree">
+        <ul class="rd-tree-node">
+            <tree-node :tree-data="treeData"></tree-node>
+        </ul>    
+    </section>
+</template>
+<script>
+import treeNode from './treeNode'
+
+export default {
+    props: {
+        treeData: Array
+    },
+    components: {
+        treeNode
+    }
+}
+</script>

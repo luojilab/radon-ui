5bd6dab5e6e7fc11c5528a008315b06dbedfff57
Awe <hilongjw@gmail.com>
2016-12-09 19:31:01 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: 'hash',
     scrollBehavior (to, from, savedPosition) {
         return { x: 0, y: 0 }
     },
